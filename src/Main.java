0778a05d25d71070bc6910a15b99ee3515ab6c6f
Anna Gergert <annya.gergert@gmail.com>
2022-01-07 20:36:45 +0300

    code fixed

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,20 @@ public class Main {
         int initialbalance;
         initialbalance = 50;
         int refill;
-        refill = 50;
-        int bonus = 0;
+        refill = 5000;
+        int bonus;
         int totalbill;
-        totalbill = initialbalance + bonus + refill;
-       if (refill > 1000) {
-        bonus = refill / 10;
-           System.out.println(bonus + " - Ваш бонус!");
-           System.out.println(totalbill + " - Ваш баланс после пополнения счёта.");
-       } else {
-           bonus = 0;
-           System.out.println("Вы пополнили счет на " +refill+ " рублей.");
-           System.out.println(totalbill + " - Ваш баланс после пополнения счёта");
 
-       }
+        if (refill > 1000) {
+            bonus = refill / 10;
+
+        } else {
+            bonus = 0;
+        }
+        totalbill = initialbalance + bonus + refill;
+
+        System.out.println("Вы пополнили счет на " + refill + " рублей.");
+        System.out.println(bonus + " - Ваш бонус!");
+        System.out.println(totalbill + " - Ваш баланс после пополнения счёта");
     }
 }
